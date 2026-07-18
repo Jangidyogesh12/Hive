ca45c1ba9ed0037142b6906336d0aa6e1fc0f777
Jangidyogesh12 <jangidyogesh123@gmail.com>
2026-07-18 17:04:59 +0530

    refactor : temp pushes for better code review

--- core/db/store_path.rs
+++ core/db/store_path.rs
@@ -1,10 +1,1 @@
-pub const NODE_STORE_FILE: &str = "nodes.hive";
-pub const EDGE_STORE_FILE: &str = "edges.hive";
-pub const PROP_STORE_FILE: &str = "props.hive";
-pub const STRING_STORE_FILE: &str = "strings.hive";
-pub const LABEL_STORE_FILE: &str = "labels.hive";
-pub const INDEX_STORE_FILE: &str = "indexes.hive";
-pub const FREE_LIST_NODE: &str = "freelist_nodes.hive";
-pub const FREE_LIST_EDGE: &str = "freelist_edges.hive";
-pub const META_FILE: &str = "meta.hive";
 pub const WAL_FILE: &str = "wal.hive";
